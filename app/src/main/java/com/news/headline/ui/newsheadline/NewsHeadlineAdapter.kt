01d2bf67a413a6e593f03cd2bdb2cccef962bc19
Palkesh Jain <Palkesh.jain@useriq.com>
2019-12-06 18:53:05 +0530

    code refactor for NewsHeadlineAdapter

--- app/src/main/java/com/news/headline/ui/newsheadline/NewsHeadlineAdapter.kt
+++ app/src/main/java/com/news/headline/ui/newsheadline/NewsHeadlineAdapter.kt
@@ -1,8 +1,9 @@
 package com.news.headline.ui.newsheadline
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.news.headline.R
@@ -11,46 +12,49 @@ import kotlinx.android.synthetic.main.item_news_headline.view.*
 
 class NewsHeadlineAdapter(
     private val newsList: List<News>,
-    private val listener: (Int) -> Unit
-) : RecyclerView.Adapter<NewsHeadlineAdapter.NewsViewHolder>() {
+    private val onNewsClicked: (id: Int) -> Unit
+) : ListAdapter<News, NewsViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_news_headline, parent, false)
-        return NewsViewHolder(view)
+        val holder = NewsViewHolder(parent)
+        holder.itemView.setOnClickListener {
+            onNewsClicked(holder.adapterPosition)
+        }
+        return holder
     }
 
-    override fun getItemCount(): Int {
-        return newsList.size
+    override fun getItem(position: Int): News {
+        return newsList[position]
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        val news = newsList[position]
+        val news = getItem(position)
         holder.ivBg.setImageDrawable(null)
 
         holder.tvHeadline.text = news.title
-        holder.tvDate.text = news.publishedAt?.substring(0,10)
-        holder.tvSource.text = news.source?.name
+        holder.tvDate.text = news.publishedAt?.substring(0, 10)
+        holder.tvSource.text = news.source.name
         holder.tvHeadline.text = news.title
         Glide.with(holder.itemView).load(news.urlToImage).into(holder.ivBg)
-
-        holder.bind(holder.container, position)
     }
 
-    inner class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+}
 
-        val container = view.container
-        val ivBg = view.iv_news_headline_bg
-        val tvHeadline = view.tv_news_headline
-        val tvDate = view.tv_news_date
-        val tvSource = view.tv_news_source
-
-        fun bind(view: View, pos: Int) {
-            view.setOnClickListener {
-                listener(pos)
-            }
-
-        }
+val DIFF_CALLBACK = object : DiffUtil.ItemCallback<News>() {
+    override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
+        return oldItem.id == newItem.id
     }
 
+    override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class NewsViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_news_headline, parent, false)
+) {
+    val ivBg = itemView.iv_news_headline_bg
+    val tvHeadline = itemView.tv_news_headline
+    val tvDate = itemView.tv_news_date
+    val tvSource = itemView.tv_news_source
 }
